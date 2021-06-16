90e34ac6be5c330fe0a5e3daa7fa5feb23f1baa4
Hannah Paxton <hannahlinnpaxton@gmail.com>
2021-06-16 17:55:58 -0400

    Added homepage images

---
+++ src/Home.js
@@ -0,0 +1,18 @@
+import logo from './logo.svg';
+import { Box } from "@chakra-ui/react"
+
+function App() {
+  return (
+
+  <Box>
+      <Box height="100vh" width="100%" backgroundImage="url('http://tastedwasted.com/images/Baileys-cupcakescrop1.jpg')" backgroundSize="cover">
+        <Box height="100vh" width="100%" backgroundImage="url('http://tastedwasted.com/images/twlogo-01copy.png')" backgroundPosition="center" backgroundRepeat="no-repeat">
+        </Box>       
+      </Box>
+      <Box height="100vh" width="100%" backgroundImage="url('http://tastedwasted.com/images/bottles.jpg')" backgroundSize="cover">
+      </Box>
+  </Box>
+  );
+}
+
+export default App;
